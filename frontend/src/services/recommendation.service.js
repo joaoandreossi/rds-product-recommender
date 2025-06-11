da1501b07267042fd161d59037b7a2c220d4bca7
joao.andreossi <joao.andreossi@fc.unesp.br>
2025-06-10 21:48:58 -0300

    feat(algo): create recommendation algorithmn and add new service tests

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -7,6 +7,34 @@ const getRecommendations = (
   /**
    * Crie aqui a lógica para retornar os produtos recomendados.
    */
+  const recommendationSet = new Set();
+  const userPreferenceSet = new Set(formData.selectedPreferences);
+  const userFeatureSet = new Set(formData.selectedFeatures);
+    
+  for(const product of products){
+    const preferenceSet = new Set(product.preferences);
+    const featureSet = new Set(product.features);
+
+    if(preferenceSet.intersection(userPreferenceSet).size > 0){
+      if(!recommendationSet.has(product)){
+        recommendationSet.add(product);
+      }
+    }
+
+    if(featureSet.intersection(userFeatureSet).size > 0){
+      if(!recommendationSet.has(product)){
+        recommendationSet.add(product);
+      }
+    }
+  }
+
+  let recommendation = [...recommendationSet.values()];
+    
+  if(recommendation.length > 0 && formData.selectedRecommendationType === 'SingleProduct') {
+    recommendation = [recommendation[recommendation.length - 1]];
+  }
+  
+  return recommendation;
 };
 
 export default { getRecommendations };
